670c183eb9b55b66799b0d0f357b30d587761062
anna-dahlberg <anna.dahlberg@live.se>
2025-05-06 15:03:22 +0200

    Added button with links form EndPage

--- src/pages/EndPage.tsx
+++ src/pages/EndPage.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 function EndPage() {
-    return (
-      <div>
-        <h1>Slutsidan</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Resultat</h1>
+      <Link to="/">
+        <button>Startsidan</button>
+      </Link>
+      <Link to="/runesbeachclub">
+        <button>Spela igen</button>
+      </Link>
+    </div>
+  );
+}
   
   export default EndPage;
