8e830c292e80ca79b9bccdb8971909481484ec4b
anna-dahlberg <anna.dahlberg@live.se>
2025-05-07 14:22:51 +0200

    defined props and structure

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -2,10 +2,19 @@ import type { ReactNode } from 'react';
 import styled from '@emotion/styled';
 import backgroundImage from './../assets/backgroundImages/game_background.png';
 
+interface WindowProps {
+    children: ReactNode;
+    maxWidth?: string;
+    maxHeight?: string;
+  }
 
-function Window() {
+function Window({ children, maxWidth, maxHeight }: WindowProps) {
   return (
-    <></>
+    <WindowContainer>
+      <WindowContent maxWidth={maxWidth} maxHeight={maxHeight}>
+        {children}
+      </WindowContent>
+    </WindowContainer>
   );
 }
 
