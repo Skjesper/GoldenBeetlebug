ecc8bc558a7105fb6ce03686f38c1aed1b3a0e14
Julia Lyngfelt <julialyngfelt@hotmail.com>
2025-05-07 10:36:38 +0200

    22 feature style turn your phone around page (#28)
    * Startpage paortrait basic styling
    
    * basic styling and cleaned up code for startpage

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,17 +1,45 @@
-import { Link } from "react-router-dom";
-import GameStart from './../components/GameStart'
+import { Link } from 'react-router-dom';
 
-function StartPage() {
-    return (
-      <div>
-        <GameStart />
-        <h1>Startsidan</h1>
-        <p>Klicka nedan för att starta spelet</p>
-        <Link to="/runesbeachclub">
-            <button>Start Game</button>
-        </Link>
-        </div>
-    );
+import styled from '@emotion/styled';
+import React, { useState, useEffect } from 'react';
+
+
+const StartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: #b69191;
+
+  @media (orientation: landscape) {
+    background: #c76565;
+    flex-direction: row;
   }
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 2rem;
+  font-weight: 300;
+  color: #384e56;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  padding: 8rem 3rem;
+  text-align: center;
   
-  export default StartPage;
+  @media (orientation: landscape) {
+    display: none;
+  }
+`;
+
+ function StartPage() {
+  return (
+    
+      <StartContainer>
+        <StyledH1>Vrid skärmen till liggande läge</StyledH1>
+      <Link to="/runesbeachclub">
+        <button>Start Game</button>
+      </Link>
+      </StartContainer>
+    
+  );
+};
+
+export default StartPage;
