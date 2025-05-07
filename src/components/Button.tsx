052797c5c544cd7d84cb4f0c5392d8f05e3eecee
Anna Dahlberg <anna.dahlberg@live.se>
2025-05-07 10:56:42 +0200

    27 feature create button component (#29)
    * Created and styled button component
    
    * Added note with button and links

---
+++ src/components/Button.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+import type { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+const StyledButton = styled.button`
+        background-color: #ea0b0b;
+        color: #ffffff;
+        border: none;
+        font-size: 1rem;
+        cursor: pointer;
+        width: fit-content;
+        padding: 0.75rem 1.5rem;
+        border-radius: 8px;
+
+        &:hover {
+            background-color: #48b300;
+        }
+
+        @media (orientation: landscape) {
+            display: none;
+        }
+        
+`;
+
+type ButtonProps = {
+    children: ReactNode;
+    to?: string;
+}
+
+function Button({ children, to }: ButtonProps) {
+    if (to) {
+        return (
+            <Link to={to}>
+                <StyledButton>{children}</StyledButton>
+            </Link>
+        );
+    }
+
+    return <StyledButton>{children}</StyledButton>;
+}
+
+{/* <Button to="/"></Button>
+<Button to="/resultat"></Button>
+<Button to="/runesbeachclub"></Button> */}
+
+export default Button;
