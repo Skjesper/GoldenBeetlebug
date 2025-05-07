9024e2be64c3d86ea29ada023fa783ce129f25e1
Skjesper <jesperskeppstedt@gmail.com>
2025-05-07 10:45:36 +0200

    timer komponent works

--- src/components/RuneHunt.tsx
+++ src/components/RuneHunt.tsx
@@ -1,15 +1,37 @@
-// import react, { useState, UseEffect } from 'react';
+import react, { useState, useEffect } from 'react';
+
+import Timer from "./Timer";
 
 
 
 export default function RuneHunt () {
 
-    
+    const [gameRunning, setGameRunning] = useState<boolean>(false);
+
+    const handleTimeOut = () => {
+        console.log("Times up");
+        setGameRunning(false);
+    }
+
+    const startGame = () => {
+        setGameRunning(true);
+      };
 
     return (
-
+        <div>
+        <Timer 
+        initialTime={30} 
+        isRunning={gameRunning} 
+        onTimeOut={handleTimeOut}
+        countDown={true}
+      />
         <h1>Detta är en H1</h1>
+      
 
+        <button onClick={startGame}>
+            {gameRunning ? "Pågår..." : "Starta spelet"}
+        </button>
+        </div>
     )
 
 
