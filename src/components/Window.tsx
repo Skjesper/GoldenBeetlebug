7c16a38232f16b1020c1fd52852c3ff0fc400e22
anna-dahlberg <anna.dahlberg@live.se>
2025-05-07 14:21:36 +0200

    created Window component

---
+++ src/components/Window.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from 'react';
+import styled from '@emotion/styled';
+import backgroundImage from './../assets/backgroundImages/game_background.png';
+
+
+function Window() {
+  return (
+    <></>
+  );
+}
+
+export default Window;
