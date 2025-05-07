aaa69279c54e36d6501db2c3fa825a172012de05
Skjesper <jesperskeppstedt@gmail.com>
2025-05-07 10:19:04 +0200

    added TS interface for Timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,16 +4,18 @@ interface TimerProps {
 
     initialTime?: number;
     isRunning: boolean;
+    onTimeChange?: (time: number) => void;
+    onTimeOut?: () => void;
+    countDown?: boolean;
 
 
 }
 
 
-export default function Timer (){
+export default function Timer({ initialTime = 60, isRunning, onTimeChange, onTimeOut, countDown = true }: TimerProps) {
 
-    initialTime = 60,
 
- 
+
 
 
 }
