5b3ca4bea51fec177fd1a862ebd2cbede7670b31
Skjesper <jesperskeppstedt@gmail.com>
2025-05-07 09:57:42 +0200

    added test div for the runehunt function

---
+++ src/components/RuneHunt.tsx
@@ -0,0 +1,17 @@
+// import react, { useState, UseEffect } from 'react';
+
+
+
+export default function RuneHunt () {
+
+    
+
+    return (
+
+        <h1>Detta är en H1</h1>
+
+    )
+
+
+}
+
---
+++ src/components/Timer.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+interface TimerProps {
+
+    initialTime?: number;
+    isRunning: boolean;
+
+
+}
+
+
+export default function Timer (){
+
+    initialTime = 60,
+
+ 
+
+
+}
