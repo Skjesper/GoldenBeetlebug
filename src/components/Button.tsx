386486516d8540bd53f1b47e49bc450cd82c724a
anna-dahlberg <anna.dahlberg@live.se>
2025-05-07 10:47:22 +0200

    Created and styled button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import type { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
+const StyledButton = styled.button`
+        background-color: #ea0b0b;
+        color: #ffffff;
+        border: none;
+        font-size: 1rem;
+        cursor: pointer;
+        width: fit-content;
+        padding: 0.75rem 1.5rem;
+        border-radius: 8px;
+
+        &:hover {
+            background-color: #48b300;
+        }
+
+        @media (orientation: landscape) {
+            display: none;
+        }
+        
+`;
+
+type ButtonProps = {
+    children: ReactNode;
+    to?: string;
+}
+
+function Button({ children, to }: ButtonProps) {
+    if (to) {
+        return (
+            <Link to={to}>
+                <StyledButton>{children}</StyledButton>
+            </Link>
+        );
+    }
+
+    return <StyledButton>{children}</StyledButton>;
+}
+
+export default Button;
