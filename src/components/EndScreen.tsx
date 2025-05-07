2e6217276cd36a7c3195a72eb52b425d08c45441
Skjesper <jesperskeppstedt@gmail.com>
2025-05-07 14:29:40 +0200

    added getResultMessage to endscreen

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-export default function EndScreen () {
 
+
+export default function EndScreen ({ score, highscore, onRestart}) {
+
+    const getResultMessage = () => {
+
+        if (score >= highScore && highScore > 0) return "Nytt rekord!";
+        if (score > 0) return "Bra spelat!";
+        return "Försök igen!";
+
+    }
 
     return (
 
-        
+
 
     )
 
