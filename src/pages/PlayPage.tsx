c39402acc5246c4a5fb9b27881cd38923cddcad4
anna-dahlberg <anna.dahlberg@live.se>
2025-05-09 09:24:49 +0200

    changed page name

---
+++ src/pages/PlayPage.tsx
@@ -0,0 +1,13 @@
+import RuneHunt from "../components/RuneHunt";
+import Window from "../components/Window";
+
+function PlayPage() {
+  
+  return (
+    <Window>
+      <RuneHunt />
+    </Window>
+  );
+}
+  
+  export default PlayPage;
