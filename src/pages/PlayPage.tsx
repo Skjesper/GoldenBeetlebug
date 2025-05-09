d27b3e743d3e6cab712252e3ed6cb5f1b4c984ed
Anna Dahlberg <anna.dahlberg@live.se>
2025-05-09 09:55:38 +0200

    52 update slugs and component names (#67)
    * updated route names
    
    * changed page name and route
    
    * updated names
    
    * changed page name
    
    * updated page name and route

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -1,7 +1,7 @@
 import RuneHunt from "../components/RuneHunt";
 import Window from "../components/Window";
 
-function GamePage() {
+function PlayPage() {
   
   return (
     <Window>
@@ -10,4 +10,4 @@ function GamePage() {
   );
 }
   
-  export default GamePage;
+  export default PlayPage;
