c2692d46dc2a31e011dbbf50c869bcbe42269800
anna-dahlberg <anna.dahlberg@live.se>
2025-05-09 11:27:43 +0200

    removed import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RuneHuntPage from "./pages/RuneHuntPage";
 import LeaderboardPage from "./pages/EndPage";
 import PlayPage from "./pages/PlayPage";
-import './App.css'
 
 function App() {
 
