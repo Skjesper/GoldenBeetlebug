dc0fb04be1316b492f108a263bf7df524ab264d3
Skjesper <jesperskeppstedt@gmail.com>
2025-05-13 11:26:09 +0200

    Added a runhuntest

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import RuneHunt from "../components/RuneHunt";
 import Window from "../components/Window";
+import DuckHunt from '../components/RuneHuntTest';
 
 function PlayPage() {
     const [backgroundImage, setBackgroundImage] = useState<string | undefined>(undefined);
 
     return (
         <Window backgroundImage={backgroundImage}>
-            <RuneHunt onBackgroundChange={setBackgroundImage} />
+          <DuckHunt />
+            {/* <RuneHunt onBackgroundChange={setBackgroundImage} /> */}
         </Window>
     );
 }
