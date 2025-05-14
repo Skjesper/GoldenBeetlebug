0171d3b1cd6706aa85d5abdfeb123279918e740b
anna-dahlberg <anna.dahlberg@live.se>
2025-05-14 11:08:38 +0200

    styled score container

--- src/components/ScoreForm.tsx
+++ src/components/ScoreForm.tsx
@@ -2,6 +2,31 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
 import Scoreboard from './ScoreBoard';
+import styled from '@emotion/styled';
+import Button from './Button';
+
+const ScoreBoardContainer = styled.div`
+    width: 22.5625rem;
+    height: 16.8125rem;
+    border-radius: 0.25rem;
+    background: #D9D9D9;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const ScoreBoardTitle = styled.h1`
+    font-size: 1.875rem;
+    font-family: var(--font-display);
+    text-align: center;
+`;
+
+const ScoreInput = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 interface ScoreFormProps {
   score?: number;
@@ -44,28 +69,30 @@ const ScoreForm: React.FC<ScoreFormProps> = ({ score = 0 }) => {
 
   return (
     <div>
-      <h2>Lägg till poäng</h2>
+      
+      <ScoreBoardContainer>
+        <ScoreBoardTitle>Grymt jobbat! <br />Vill du spara ditt resutlat?</ScoreBoardTitle>
+          <ScoreInput onSubmit={handleSubmit}>
+              <Scoreboard score={points} />
+              <input
+                type="text"
+                id="nickname"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+                required
+                placeholder='Nickname'
+              />
 
-      {message && <p>{message}</p>}
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="nickname">Nickname:</label>
-          <input
-            type="text"
-            id="nickname"
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
-            required
-          />
-        </div>
+            <Button type="submit" disabled={loading}>
+              {loading ? 'Sparar...' : 'Spara'}
+            </Button>
 
-        <Scoreboard score={points} />
+          </ScoreInput>
+          {message && <p>{message}</p>}
+      </ScoreBoardContainer>
 
-        <button type="submit" disabled={loading}>
-          {loading ? 'Sparar...' : 'Spara poäääääng'}
-        </button>
-      </form>
+
     </div>
   );
 };
