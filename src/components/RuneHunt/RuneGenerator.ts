be6694edc412874cb4a26a51ef601428996b4049
Skjesper <jesperskeppstedt@gmail.com>
2025-05-19 10:25:53 +0200

    Moved over some generation logic to its own ts file.

---
+++ src/components/RuneHunt/RuneGenerator.ts
@@ -0,0 +1,14 @@
+import Rune from "./Rune";
+
+
+export class RuneGenerator {
+
+    private gameWidth: number;
+    private gameHeight: number;
+
+    constructor(gameWidth: number, gameHeight: number) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+      }
+
+}
