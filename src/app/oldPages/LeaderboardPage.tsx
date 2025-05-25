28b66fed9d0b8643cba7d19595aa8c52939b3849
Skjesper <jesperskeppstedt@gmail.com>
2025-05-25 08:27:47 +0200

    Wrapped OrientationOverlay to leaderboardPage
    Co-Authored-By: Anna Dahlberg <anna.dahlberg@live.se>
    Co-Authored-By: Julia Lyngfelt <168407644+jlyngfelt@users.noreply.github.com>

--- src/app/oldPages/LeaderboardPage.tsx
+++ src/app/oldPages/LeaderboardPage.tsx
@@ -1,15 +1,15 @@
 
 'use client'
 import { useEffect, useState } from "react";
-import Window from "../components/Window";
 import Leaderboard from "../components/Leaderboard";
 import ScoreForm from '../components/ScoreForm';
 import styled from "@emotion/styled";
+import OrientationOverlay from "../components/OrientationCheck/OrientationCheck";
 
 
 const PageContainer = styled.div`
     width: 100%;
-    height: 100%;
+    height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -29,7 +29,7 @@ function LeaderboardPage() {
 
   return (
      <div>
-   <Window backgroundImage={backgroundImage}>
+   <OrientationOverlay>
       <PageContainer>
      {!display ? (
        <ScoreForm score={gameScore}  onDisplayChange={setDisplay}/>
@@ -37,7 +37,7 @@ function LeaderboardPage() {
         <Leaderboard/>
       )}
       </PageContainer>
-   </Window>
+   </OrientationOverlay>
  </div>
   );
 
